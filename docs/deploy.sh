a914c449f54fbbd1f42d06ee0b7573dfe3c2d5b9
goodsj <goodsj@gmail.com>
2021-04-23 14:26:31 +0900

    deploy

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:goodsj/goodsj.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://goodsj.github.com/ master:gh-pages
+git push -f git@github.com:goodsj/goodsj.github.io.git master
 
 
 cd -
