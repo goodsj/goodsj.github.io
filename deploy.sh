2b71fe31c1e68329858b1bdabca8383e255bcd94
goodsj <goodsj@gmail.com>
2021-04-23 14:30:29 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run docs:build
+npm run build
 
 # navigate into the build output directory
 cd docs/.vuepress/dist
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:goodsj/goodsj.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/goodsj/goodsj.github.io.git master:gh-pages
-
+# git push -f https://github.com/goodsj/goodsj.github.io.git master:gh-pages
+git push -f git@github.com:goodsj/goodsj.github.io.git master
 
 cd -
